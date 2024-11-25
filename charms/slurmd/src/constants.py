439024e2b5f31bd6a25afa499719b77a52c9e8c8
jamesbeedy <jamesbeedy@users.noreply.github.com>
2024-11-25 12:53:27 -0800

    Delete charms/slurmd/src/constants.py

--- charms/slurmd/src/constants.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2024 Omnivector, LLC.
-# See LICENSE file for licensing details.
-
-"""This module provides constants for the slurmd-operator charm."""
-
-CHARM_MAINTAINED_NODE_PARAMETERS = {"MemSpecLimit": "1024"}
