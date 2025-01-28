f5d90fc8f8cd3689f0d1c8a6f66b25481b28b2d6
Dominic Sloan-Murphy <dominic.sloanmurphy@canonical.com>
2025-01-28 10:43:00 +0000

    feat(slurmd): re-enable UCX transport for OpenMPI

--- charms/slurmd/src/utils/rdma.py
+++ charms/slurmd/src/utils/rdma.py
@@ -37,8 +37,50 @@ def install() -> None:
         RDMAOpsError: Raised if error is encountered during package install.
     """
     install_packages = ["rdma-core", "infiniband-diags"]
+
     _logger.info("installing RDMA packages: %s", install_packages)
     try:
         apt.add_package(install_packages)
     except (apt.PackageNotFoundError, apt.PackageError) as e:
         raise RDMAOpsError(f"failed to install packages {install_packages}. reason: {e}")
+
+    # Re-enable UCX/UCT.
+    # The OpenMPI package from archive includes a Debian patch that disables UCX to silence
+    # warnings when running on systems without RDMA hardware. This leads to the less performant
+    # "ob1" method being selected on Infiniband-enabled systems. By default, OpenMPI selects UCX
+    # when Infiniband devices are available. This default functionality is restored here.
+    #
+    # See:
+    # https://sources.debian.org/src/openmpi/4.1.4-3/debian/patches/no-warning-unused.patch/#L24
+    # https://github.com/open-mpi/ompi/issues/8367
+    # https://github.com/open-mpi/ompi/blob/v4.1.x/README#L763
+    path = "/etc/openmpi/openmpi-mca-params.conf"
+    _logger.info("enabling OpenMPI UCX transport in %s", path)
+
+    try:
+        with open(path, "r") as f:
+            lines = f.readlines()
+    except FileNotFoundError:
+        _logger.warn("%s not found. skipping OpenMPI UCX enablement", path)
+        return
+
+    # Current patch adds lines:
+    #   btl = ^uct,openib,ofi
+    #   pml = ^ucx
+    #   osc = ^ucx,pt2pt
+    # Remove "uct" and "ucx". Values must still begin with "^", e.g. `btl = ^openib,ofi`, to leave
+    # openib and ofi disabled.
+    new_lines = []
+    for line in lines:
+        if line.startswith(("btl =", "pml =", "osc =")):
+            parts = line.split()
+            values = parts[2].strip().lstrip("^").split(",")
+            values = [v for v in values if v not in ("uct", "ucx")]
+            # Remove line entirely if all values removed, e.g. "pml = ^ucx"
+            if values:
+                new_lines.append(f"{parts[0]} = ^{','.join(values)}\n")
+        else:
+            new_lines.append(line)
+
+    with open(path, "w") as f:
+        f.writelines(new_lines)
